8fe02ff9badc819bf1a1cfc0841cbbdc4853bc79
akshayblevel <akshayblevel@gmail.com>
2021-08-20 18:00:54 +0530

    3 files modified, 3 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+export class AppComponent {}
